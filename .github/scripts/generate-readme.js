14e76c5eb6aaaebd7eae8214c2c8672262cdf871
yphs31 <111676992+yphs31@users.noreply.github.com>
2023-04-09 16:41:44 +0800

    Update generate-readme.js

--- .github/scripts/generate-readme.js
+++ .github/scripts/generate-readme.js
@@ -1,23 +1,23 @@
 const fs = require('fs');
 
 // 遞歸函數遍歷目錄結構，輸出到README.md中
-function generateReadme(path, level, count) {
+function generateReadme(path, level) {
   const files = fs.readdirSync(path);
   let output = '';
   level = level || 0;
   const indent = '  '.repeat(level); // 每一層的縮進
 
+  let count = 0;
   files.forEach(function(file) {
     const stats = fs.statSync(path + '/' + file);
     if (stats.isDirectory()) {
-      if(!file.startsWith('.')) {
-        // 如果是資料夾，遞歸處理
-        output += indent + '- ' + file + '\t';
-        const result = generateReadme(path + '/' + file, level + 1, count);
-        output += `【${result.count}】\n`;
-        output += result.output;
-        count = result.count;
+      if(file.startsWith('.')) {
+        return;
       }
+      // 如果是資料夾，遞歸處理
+      const result = generateReadme(path + '/' + file, level + 1);
+      output += indent + `- ${file} (${result.count} files)\n`;
+      count += result.count;
     } else {
       // 如果是檔案，輸出檔名
       if (file.endsWith('.cpp') || file.endsWith('.py') || file.endsWith('.c')) {
@@ -30,11 +30,11 @@ function generateReadme(path, level, count) {
 }
 
 // 生成程式碼區塊
-const result = generateReadme('./', 0, 0);
+const result = generateReadme('./', 0);
 const readmeContent = `
-# 延平資訊研究社第27屆
+# Howard-OJ
 
-###### 紀錄上課資料
+###### 專門紀錄我的程式資料夾。
 
 \`總共完成了 ${result.count} 個題目。\`
 
