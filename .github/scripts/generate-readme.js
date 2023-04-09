b73203a5d2c0713f7e9c8deed0be80c45483f435
yphs31 <111676992+yphs31@users.noreply.github.com>
2023-04-09 16:59:54 +0800

    Update generate-readme.js

--- .github/scripts/generate-readme.js
+++ .github/scripts/generate-readme.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 
 // 遞歸函數遍歷目錄結構，輸出到README.md中
-function generateReadme(path, level, count = 0) {
+function generateReadme(path, level) {
   const files = fs.readdirSync(path);
   let output = '';
+  let count = 0;
   level = level || 0;
-  const indent = '  '.repeat(level);
+  const indent = '  '.repeat(level); // 每一層的縮進
 
   files.forEach(function(file) {
     const stats = fs.statSync(path + '/' + file);
@@ -13,35 +14,22 @@ function generateReadme(path, level, count = 0) {
       if(file.startsWith('.')) {
         return;
       }
+      // 如果是資料夾，遞歸處理
       const result = generateReadme(path + '/' + file, level + 1);
-      const numCppFiles = result.count;
-      count += numCppFiles;
-      const countStr = numCppFiles ? ` (${numCppFiles} files)` : '';
-      output += `${indent}- ${file}${countStr}\n${result.output}`;
-    } else if (file.endsWith('.cpp') || file.endsWith('.py') || file.endsWith('.c')) {
-      count++;
-      output += `${indent}- ${file}\n`;
+      const folderCount = result.count;
+      count += folderCount;
+      output += indent + `- ${file} (${folderCount} files)\n${result.output}`;
+    } else {
+      // 如果是檔案，輸出檔名
+      if (file.endsWith('.cpp') || file.endsWith('.py') || file.endsWith('.c')) {
+        count++;
+        output += indent + '  - `' + file + '`\n';
+      }
     }
   });
 
   return { output: output, count: count };
 }
-
-const result = generateReadme('./', 0);
-const readmeContent = `
-# Howard-OJ
-
-###### 專門紀錄我的程式資料夾。
-
-\`總共完成了 ${result.count} 個題目。\`
-
-\`\`\` 
-${result.output}\`\`\`
-`;
-
-fs.writeFileSync('README.md', readmeContent);
-
-
 
 // 生成程式碼區塊
 const result = generateReadme('./', 0);
