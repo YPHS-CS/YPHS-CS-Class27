1231e03ecacf267c1019264a1b0c18ac9bc26b28
User-Howard <howard20060216@gmail.com>
2023-06-03 14:01:21 +0800

    _

--- .github/scripts/generate-readme.js
+++ .github/scripts/generate-readme.js
@@ -34,9 +34,7 @@ function generateReadme(path, level) {
 // 生成程式碼區塊
 const result = generateReadme('./', 0);
 const readmeContent = `
-# YPHS-IRCS-Class27
-這個倉庫是關於台北市私立延平高級中學第27屆資訊研究社社課教學內容的資料庫。我們收集和整理了社課期間所學的各種資料、課程講義、示例代碼和項目案例，旨在為社員提供學習資源和參考資料。
-
+<!-- include-markdown: docs/Guide.md -->
 
 <details><summary>架構圖</summary>
 <p>
