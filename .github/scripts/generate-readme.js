52427a2e978bfb711224831a7bcf9d3fcbbaf62b
Hao-wei Wu <60650989+User-Howard@users.noreply.github.com>
2023-06-03 13:55:48 +0800

    Update generate-readme.js

--- .github/scripts/generate-readme.js
+++ .github/scripts/generate-readme.js
@@ -35,7 +35,7 @@ function generateReadme(path, level) {
 const result = generateReadme('./', 0);
 const readmeContent = `
 # YPHS-IRCS-Class27
-這個倉庫是關於台北市私立延平高級中學第27屆資訊研究社社課教學內容的資料庫。我們收集和整理了社課期間所學的各種資料、課程講義、示例代碼和項目案例，旨在為社員提供學習資源和參考資料。
+這個倉庫是關於台北市私立延平高級中學第27屆資訊研究社社課教學內容的資料庫。我們收集和整理了社課期間所學的各種資料、課程講義和示例代碼旨在為社員提供學習資源和參考資料。
 
 
 <details><summary>架構圖</summary>
